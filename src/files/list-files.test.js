6fb253b036150157151731a21e4176b160a9d56b
Paulio <pauliorandall@gmail.com>
2023-11-05 15:41:18 +0000

    Move file listing func to its own file + create testdata util file

---
+++ src/files/list-files.test.js
@@ -0,0 +1,18 @@
+import testdata from './testdata.js'
+import { listP69Files } from './list-files.js'
+
+describe('list-files', () => {
+	test('correctly lists all .p69 testdata files', async () => {
+		await testdata.reset()
+
+		const act = await listP69Files(testdata.root)
+		const exp = testdata.files
+			.filter((f) => f.format === 'p69')
+			.map((f) => f.path)
+
+		act.sort()
+		exp.sort()
+
+		expect(act).toEqual(exp)
+	}, 2000)
+})
---
+++ src/files/testdata.js
@@ -0,0 +1,85 @@
+import fs from 'fs'
+import path from 'path'
+
+const newPath = (dir, f) => {
+	f = path.join(dir, f)
+	return path.resolve(f)
+}
+
+const root = './src/files/testdata'
+const files = [
+	{
+		path: newPath(root, './alpha/alpha.html'),
+		format: 'html',
+		content: '',
+	},
+	{
+		path: newPath(root, './alpha/alpha.p69'),
+		format: 'p69',
+		content: '.alpha {\n\tcolor: $color;\n}\n',
+	},
+	{
+		path: newPath(root, './alpha/beta/beta.html'),
+		format: 'html',
+		content: '',
+	},
+	{
+		path: newPath(root, './alpha/beta/beta.p69'),
+		format: 'p69',
+		content: '.beta {\n\tpadding: $pad;\n}\n',
+	},
+	{
+		path: newPath(root, './alpha/charlie/charlie.html'),
+		format: 'html',
+		content: '',
+	},
+	{
+		path: newPath(root, './alpha/charlie/charlie.p69'),
+		format: 'p69',
+		content: '.charlie {\n\tcolor: $color;\n\tpadding: $pad;\n}\n',
+	},
+	{
+		path: newPath(root, './alpha/charlie/other.css'),
+		format: 'css',
+		content: '.other {\n\tcolor: green;\n}\n',
+	},
+]
+
+const reset = async () => {
+	await purge()
+	await create()
+}
+
+const purge = async () => {
+	await fs.promises.rm(root, {
+		recursive: true,
+		force: true,
+	})
+}
+
+const create = async () => {
+	for (const f of files) {
+		await createFile(f.path, f.content)
+	}
+}
+
+const createFile = async (filepath, content) => {
+	const parent = path.dirname(filepath)
+	await fs.promises.mkdir(parent, { recursive: true })
+	await fs.promises.writeFile(filepath, content, { encoding: 'utf-8' })
+}
+
+const sleep = (timeout) => {
+	return new Promise((resolve) => {
+		setTimeout(resolve, timeout)
+	})
+}
+
+export default {
+	root,
+	files,
+	reset,
+	purge,
+	create,
+	sleep,
+}
